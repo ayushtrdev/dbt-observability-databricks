23c73ae4b6180339bcea12c76273356474841753
ayushtripathi@deloitte.com <ayushtripathi@deloitte.com>
2026-03-19 14:15:00 +0000

    new

--- models/marts/dim_dates.sql
+++ models/marts/dim_dates.sql
@@ -4,21 +4,17 @@
     tags=['marts', 'dimensions']
 ) }}
 
-WITH date_range AS (
-    SELECT
-        DATE_ADD(DATE('2023-01-01'), INTERVAL CAST(ROW_NUMBER() OVER (ORDER BY 1) - 1 AS INT) DAY) as date_key
-    FROM RANGE(730)  -- 2 years of dates
-)
-
 SELECT
-    date_key,
-    CAST(YEAR(date_key) AS STRING) as year,
-    CAST(MONTH(date_key) AS STRING) as month,
-    CAST(QUARTER(date_key) AS STRING) as quarter,
-    CAST(DAYOFWEEK(date_key) AS STRING) as day_of_week,
-    DAYNAME(date_key) as day_name,
-    MONTHNAME(date_key) as month_name,
-    WEEKOFYEAR(date_key) as week_of_year,
-    CASE WHEN DAYOFWEEK(date_key) IN (1, 7) THEN 'Weekend' ELSE 'Weekday' END as weekend_flag,
+    DATE_ADD(DATE('2023-01-01'), INTERVAL CAST(n AS INT) DAY) as date_key,
+    YEAR(DATE_ADD(DATE('2023-01-01'), INTERVAL CAST(n AS INT) DAY)) as year,
+    MONTH(DATE_ADD(DATE('2023-01-01'), INTERVAL CAST(n AS INT) DAY)) as month,
+    QUARTER(DATE_ADD(DATE('2023-01-01'), INTERVAL CAST(n AS INT) DAY)) as quarter,
+    DAYOFWEEK(DATE_ADD(DATE('2023-01-01'), INTERVAL CAST(n AS INT) DAY)) as day_of_week,
+    DAYNAME(DATE_ADD(DATE('2023-01-01'), INTERVAL CAST(n AS INT) DAY)) as day_name,
+    MONTHNAME(DATE_ADD(DATE('2023-01-01'), INTERVAL CAST(n AS INT) DAY)) as month_name,
+    WEEKOFYEAR(DATE_ADD(DATE('2023-01-01'), INTERVAL CAST(n AS INT) DAY)) as week_of_year,
+    CASE WHEN DAYOFWEEK(DATE_ADD(DATE('2023-01-01'), INTERVAL CAST(n AS INT) DAY)) IN (1, 7) THEN 'Weekend' ELSE 'Weekday' END as weekend_flag,
     CURRENT_TIMESTAMP() as updated_at
-FROM date_range
+FROM (
+    SELECT EXPLODE(SEQUENCE(0, 729)) as n
+)
